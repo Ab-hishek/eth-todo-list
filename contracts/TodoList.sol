dd56e7d356e6406943df82f6d1f84642e0272f8d
Abhishek Agarwal <abhishek.agarwal@mayadata.io>
2022-01-04 18:15:45 +0530

    list tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,22 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint256 public taskCount = 0;
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    // Similar to hashmap in java
+    mapping(uint256 => Task) public tasks;
+
+    constructor() public {
+        createTask("Checkout your Github Profile");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
